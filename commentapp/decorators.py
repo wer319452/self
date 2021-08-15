a9e6af9ce22011933cf552fcab1f37ff6f517ae1
alsdn <alsdn319452@gmail.com>
2021-08-15 22:57:25 +0900

    self study 27
    에러 고침 detail -> delete 바보다
    
    모바일 호환 가능한 interface 생성

--- commentapp/decorators.py
+++ commentapp/decorators.py
@@ -4,8 +4,8 @@ from commentapp.models import Comment
 
 def comment_ownership_required(func):
     def decorated(request, *args, **kwargs):
-        comment = Comment.objects.get(pk=kwargs['pk'])
-        if not comment.writer == request.user:
+        target_comment = Comment.objects.get(pk=kwargs['pk'])
+        if not target_comment.writer == request.user:
             return HttpResponseForbidden()
         return func(request, *args, **kwargs)
     return decorated
--- commentapp/views.py
+++ commentapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, DeleteView
 
 from articleapp.models import Article
 from commentapp.decorators import comment_ownership_required
@@ -28,9 +28,9 @@ class CommentCreateView(CreateView):
         return reverse('articleapp:detail', kwargs={'pk':self.object.article.pk})
 
 
-# @method_decorator(comment_ownership_required, 'get')
-# @method_decorator(comment_ownership_required, 'post')
-class CommentDeleteView(DetailView):
+@method_decorator(comment_ownership_required, 'get')
+@method_decorator(comment_ownership_required, 'post')
+class CommentDeleteView(DeleteView):
     model = Comment
     context_object_name = 'target_comment'
     template_name = 'commentapp/delete.html'
