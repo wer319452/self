ee5ef63e5246a6acf2b481ce1230a5429fc7d790
alsdn <alsdn319452@gmail.com>
2021-08-21 10:47:50 +0900

    self study 27
    add projectapp
    
    except articles in project
    
    we completed(model, view, form etc..)

---
+++ projectapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=20, null=False)
+    description = models.TextField(max_length=255, null=True)
+    image = models.ImageField(upload_to='project/', null=False)
+    created_at = models.DateField(auto_now_add=True)
