2c3cf5e0d01cecd710dd708310116f04d229948a
alsdn <alsdn319452@gmail.com>
2021-09-02 12:23:50 +0900

    self study 32

--- articleapp/models.py
+++ articleapp/models.py
@@ -7,7 +7,7 @@ from projectapp.models import Project
 
 class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
-    project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name='article', null=True)
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name='article', null=True, blank= True)
 
 
     title = models.CharField(max_length=200, null=True)
--- subscribeapp/models.py
+++ subscribeapp/models.py
@@ -6,8 +6,8 @@ from projectapp.models import Project
 
 
 class Subscription(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='subscrition')
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='subscrition')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='subscription')
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='subscription')
 
     class Meta:
         unique_together = ('user', 'project')
