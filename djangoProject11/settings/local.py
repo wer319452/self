30b967f1becd9bf9803c3ba27f84500acbe473cc
alsdn <alsdn319452@gmail.com>
2021-09-28 16:39:52 +0900

    self study 46 ??dd

--- djangoProject11/settings/local.py
+++ djangoProject11/settings/local.py
@@ -31,6 +31,6 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
